e467678660317549826020555a6d779b0a870893
Thanh <thanhphamnl@gmail.com>
2020-04-30 23:07:33 -0700

    Update program 3

--- TestGit/src/Hello.java
+++ TestGit/src/Hello.java
@@ -13,7 +13,7 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is the first git program1");
+		System.out.println("This is the first git program3");
 	}
 
 }
