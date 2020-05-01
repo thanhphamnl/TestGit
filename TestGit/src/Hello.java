d6b724d12f16c0d4f9e8866d85b9f047f12811dd
Thanh <thanhphamnl@gmail.com>
2020-04-30 20:31:38 -0700

    Initial HelloWorld

---
+++ TestGit/src/Hello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author thanh
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is the first git program");
+	}
+
+}
